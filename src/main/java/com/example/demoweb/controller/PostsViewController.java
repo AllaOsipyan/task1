92ff3181292ef1914f593bf60f864a42519df230
AllaOsipyan <allosipyan@mail.ru>
2019-09-08 01:58:44 +0500

    second commit

--- src/main/java/com/example/demoweb/controller/PostsViewController.java
+++ src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -15,4 +15,9 @@ public class PostsViewController {
     return "Здесь будет главная страница";
    }
 
+    @ResponseBody
+    @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
+    public String single(@PathVariable("id") Long id) {
+        return "Здесь будет страница поста №" + id;
+    }
 }
