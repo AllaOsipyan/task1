e4d1e9cd139ffe894350d6109bcb6cb86dbe9b30
AllaOsipyan <allosipyan@mail.ru>
2019-09-17 19:37:14 +0500

    sixth commit

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class Post {
     private String text;
-    private Integer likes = 3000;
+    private Integer likes = 0;
     private Date creationDate;
 
     public Post(String text, Date creationDate){
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -4,15 +4,20 @@ import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
 @Service
 public class PostService {
+    ArrayList<Post> posts = new ArrayList<Post>(Arrays.asList(new Post("Добро пожаловать!", new Date()),
+                                                              new Post("Сегодня хорошая погода.", new Date()),
+                                                              new Post("На небе ни облачка", new Date())));
+
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
+    }
+
     public ArrayList<Post> listOfPosts(){
-        ArrayList<Post> posts = new ArrayList();
-        posts.add( new Post("Добро пожаловать!", new Date()));
-        posts.add(new Post("Сегодня хорошая погода.", new Date()));
-        posts.add(new Post("На небе ни облачка", new Date()));
         return posts;
     }
 }
